45c01ac9206083fbf949d7f6f2f8df502c1eb918
Marko Banušić <mbanusic@gmail.com>
2026-05-21 21:46:48 +0200

    update to 2.2.2

--- neznam-atproto-share.php
+++ neznam-atproto-share.php
@@ -32,7 +32,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'NEZNAM_ATPROTO_SHARE_VERSION', '2.2.1' );
+define( 'NEZNAM_ATPROTO_SHARE_VERSION', '2.2.2' );
 
 /**
  * The code that runs during plugin activation.
